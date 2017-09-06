d7060093858662d027b589b4f552b91291dd03d2
JerryJin93 <jerry93@foxmail.com>
2017-09-07 01:38:48 +0800

    Chapter 2, all exercises are done.

---
+++ Chapter 2/Ex14.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+/*
+	在你的计算机平台上用C++程序计算表达式(x+1)*(++x)+(x++)
+*/
+int main()
+{
+	int expression;
+	int x;
+	cout << "需要计算的表达式是：(x+1)*(++x)+(x++)\n\n";
+	cout << "请输入x的值：";
+	cin >> x;
+	expression = (x+1)*(++x)+(x++);
+	cout << "\n表达式的结果是：" << expression << "\n\n";
+	system("pause");
+	return 0;
+}
